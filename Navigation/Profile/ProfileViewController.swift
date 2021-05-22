697db2bd552a486833aa37e0747d425e910f2448
Iuliia Vlkv <yu.wlkw@gmail.com>
2021-05-22 13:41:03 +0500

    Edited ProfileViewController

--- Navigation/Profile/ProfileViewController.swift
+++ Navigation/Profile/ProfileViewController.swift
@@ -9,22 +9,21 @@
 import UIKit
 
 class ProfileViewController: UIViewController {
+    
+    private let header: ProfileHeaderView = ProfileHeaderView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Изменить цвет фона
         view.backgroundColor = .lightGray
+        // Добавить экземпляр класса ProfileHeaderView как subview
+        view.addSubview(header)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        // Задать frame, равный frame корневого view
+        header.frame = view.frame
     }
-    */
 
 }
