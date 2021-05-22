937385d634fa176c484aa1540a03d8489f0f6025
Iuliia Vlkv <yu.wlkw@gmail.com>
2021-05-22 12:12:56 +0500

    Added ProfileViewController and ProfileHeaderView

---
+++ Navigation/Profile/ProfileHeaderView.swift
@@ -0,0 +1,21 @@
+//
+//  ProfileHeaderView.swift
+//  Navigation
+//
+//  Created by Iuliia Volkova on 22.05.2021.
+//  Copyright © 2021 Artem Novichkov. All rights reserved.
+//
+
+import UIKit
+
+class ProfileHeaderView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Navigation/Profile/ProfileViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ProfileViewController.swift
+//  Navigation
+//
+//  Created by Iuliia Volkova on 22.05.2021.
+//  Copyright © 2021 Artem Novichkov. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
