9ec1b61d2d480d119f4c10a2dd5bd738d2a1a905
Iuliia Vlkv <yu.wlkw@gmail.com>
2021-05-25 09:07:20 +0500

    Random fixes

--- Navigation/Profile/ProfileHeaderView.swift
+++ Navigation/Profile/ProfileHeaderView.swift
@@ -26,38 +26,36 @@ class ProfileHeaderView: UIView {
         addSubview(userStatus)
         addSubview(statusButton)
         addSubview(statusField)
+        
+        setUI()
+    
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    override func layoutSubviews() {
+    func setUI(){
         // Наводим красоту в UI-элементах
         // Красота для progileImage
         profileImage.image = UIImage(named: "angryCat")
-        profileImage.frame = CGRect(x: self.safeAreaInsets.left + 16, y: self.safeAreaInsets.top + 16, width: 110, height: 110)
         profileImage.contentMode = .scaleAspectFill
         profileImage.layer.borderWidth = 3
         profileImage.layer.borderColor = UIColor.white.cgColor
-        profileImage.layer.cornerRadius = profileImage.frame.size.height/2
         profileImage.clipsToBounds = true
         
         // Красота для userName
-        userName.frame = CGRect(x: profileImage.frame.maxX + 16, y: self.safeAreaInsets.top + 27, width: self.frame.width - self.safeAreaInsets.left - self.safeAreaInsets.right - 48 - profileImage.frame.maxX, height: 20)
         userName.text = "Gavryusha the Cat"
         userName.font = UIFont.systemFont(ofSize: 18, weight: .bold)
         userName.textColor = .black
 
         // Красота для userStatus
-        userStatus.frame = CGRect(x: profileImage.frame.maxX + 16, y: self.safeAreaInsets.top + 27 + userName.frame.height + 16, width: self.frame.width - self.safeAreaInsets.left - self.safeAreaInsets.right - profileImage.frame.maxX - 48, height: 14)
         userStatus.text = "pew pew madafakas"
         userStatus.font = UIFont.systemFont(ofSize: 14, weight: .regular)
         userStatus.textColor = .gray
 
         // Красота для statusButton
-        statusButton.frame = CGRect(x: self.safeAreaInsets.left + 16, y: profileImage.frame.maxY + 16, width: self.bounds.width - self.safeAreaInsets.left - self.safeAreaInsets.right - 32, height: 50)
-        statusButton.setTitle("Show status", for: .normal)
+        statusButton.setTitle("Set status", for: .normal)
         statusButton.setTitleColor(.white, for: .normal)
         statusButton.layer.backgroundColor = UIColor.blue.cgColor
         statusButton.layer.cornerRadius = 14
@@ -68,7 +66,6 @@ class ProfileHeaderView: UIView {
         statusButton.addTarget(self, action: #selector(isPressed), for: .touchUpInside)
         
         // Красота для statusField
-        statusField.frame = CGRect(x: profileImage.frame.maxX + 16, y: statusButton.frame.minY - 16 - statusField.frame.height, width: self.frame.width - self.safeAreaInsets.left - self.safeAreaInsets.right - 48 - profileImage.frame.width, height: 40)
         statusField.layer.backgroundColor = UIColor.white.cgColor
         statusField.layer.borderWidth = 1
         statusField.layer.borderColor = UIColor.black.cgColor
@@ -78,9 +75,22 @@ class ProfileHeaderView: UIView {
         statusField.textAlignment = .natural
         statusField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: statusField.frame.height))
         statusField.leftViewMode = .always
+        // На макете не видно, есть ли placeholder, но я добавила
         statusField.placeholder = "Today I feel like..."
         statusField.addTarget(self, action: #selector(statusTextChanged(_:)), for: .editingChanged)
+    
     }
+    
+    override func layoutSubviews() {
+        // Считаем фреймы
+        profileImage.frame = CGRect(x: self.safeAreaInsets.left + 16, y: self.safeAreaInsets.top + 16, width: 110, height: 110)
+        profileImage.layer.cornerRadius = profileImage.frame.size.height/2
+        userName.frame = CGRect(x: profileImage.frame.maxX + 16, y: self.safeAreaInsets.top + 27, width: self.frame.width - self.safeAreaInsets.left - self.safeAreaInsets.right - 48 - profileImage.frame.maxX, height: 20)
+        userStatus.frame = CGRect(x: profileImage.frame.maxX + 16, y: self.safeAreaInsets.top + 27 + userName.frame.height + 16, width: self.frame.width - self.safeAreaInsets.left - self.safeAreaInsets.right - profileImage.frame.maxX - 48, height: 14)
+        statusButton.frame = CGRect(x: self.safeAreaInsets.left + 16, y: profileImage.frame.maxY + 16, width: self.bounds.width - self.safeAreaInsets.left - self.safeAreaInsets.right - 32, height: 50)
+        statusField.frame = CGRect(x: profileImage.frame.maxX + 16, y: statusButton.frame.minY - 16 - statusField.frame.height, width: self.frame.width - self.safeAreaInsets.left - self.safeAreaInsets.right - 48 - profileImage.frame.width, height: 40)
+    }
+    
     // Функция для обработки нажатия на кнопку
     @objc func isPressed() {
         // Меняем текст
