3b001616e722c0a9439074bee1cb0d58f6efabc3
Iuliia Vlkv <yu.wlkw@gmail.com>
2021-07-04 16:13:23 +0500

    Steps 1-4. Created ColorSet, added logo, created LogInViewController

---
+++ Navigation/Profile/LogInViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LoginViewController.swift
+//  Navigation
+//
+//  Created by Iuliia Volkova on 04.07.2021.
+//  Copyright © 2021 Artem Novichkov. All rights reserved.
+//
+
+import UIKit
+
+class LogInViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
