7a316f41eedb222646e639e9a7481d3046cc0228
Iuliia Vlkv <yu.wlkw@gmail.com>
2021-07-13 13:29:28 +0500

    Trying to fix keyboard

--- Navigation/Profile/LogInViewController.swift
+++ Navigation/Profile/LogInViewController.swift
@@ -138,6 +138,32 @@ class LogInViewController: UIViewController, UITextFieldDelegate {
         field.returnKeyType = UIReturnKeyType.done
     }
     
+//    override func viewWillAppear(_ animated: Bool) {
+//        super.viewWillAppear(animated)
+//
+//        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+//        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+//    }
+//
+//    override func viewDidDisappear(_ animated: Bool) {
+//        super.viewDidDisappear(animated)
+//
+//        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+//        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+//    }
+//
+//    @objc fileprivate func keyboardWillShow(notification: NSNotification) {
+//        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+//
+//            scroll.contentInset.bottom = keyboardSize.height
+//            scroll.verticalScrollIndicatorInsets = UIEdgeInsets(top: 0, left: 0, bottom: keyboardSize.height, right: 0)
+//        }
+//    }
+//
+//    @objc fileprivate func keyboardWillHide(notification: NSNotification) {
+//        scroll.contentInset.bottom = .zero
+//        scroll.verticalScrollIndicatorInsets = .zero
+//    }
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
@@ -164,7 +190,6 @@ class LogInViewController: UIViewController, UITextFieldDelegate {
         scroll.contentInset.bottom = .zero
         scroll.verticalScrollIndicatorInsets = .zero
     }
-
 }
 
 extension UIImage {
