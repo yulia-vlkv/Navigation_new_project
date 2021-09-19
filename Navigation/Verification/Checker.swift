6a5089688cb69932d1c843c3e23a93bbe453e66a
Iuliia Vlkv <yu.wlkw@gmail.com>
2021-09-19 09:55:31 +0500

    Minor fixes

--- Navigation/Verification/Checker.swift
+++ Navigation/Verification/Checker.swift
@@ -28,13 +28,9 @@ class Checker {
         let currentInfo = "\(filledInLogin)\(filledInPassword)".hash
         if currentInfo == correctInfo {
             print("Login and password are correct")
-            print(currentInfo)
-            print(correctInfo)
             return true
         } else {
             print("There's a mistake")
-            print(currentInfo)
-            print(correctInfo)
             return false
         }
     }
