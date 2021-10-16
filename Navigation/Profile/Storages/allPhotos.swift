334d81694b6237e98c079807c6123a6da7084b87
Iuliia Vlkv <yu.wlkw@gmail.com>
2021-10-16 12:50:21 +0500

    Added func addImagesWithTimer and it seems to work

--- Navigation/Profile/Storages/allPhotos.swift
+++ Navigation/Profile/Storages/allPhotos.swift
@@ -11,28 +11,28 @@ import UIKit
 // Удалила структуру imageVK, теперь это просто массив картинок. Пришлось править PhotosCollectionViewCell, PhotosViewController, PhotosTableViewCell, но вроде ничего не забыла.
 
 struct allPhotos {
-    static let photoArray: [UIImage?] = [
-        UIImage(named: "BabyStarTantrums"),
-        UIImage(named: "BlackEyeGalaxy"),
-        UIImage(named: "CaptivatingSpiral"),
-        UIImage(named: "ClusterOfRedWhiteBlue"),
-        UIImage(named: "CometNearJupitersAsteroids"),
-        UIImage(named: "CosmicNecklace"),
-        UIImage(named: "DazzlingDynamicDuo"),
-        UIImage(named: "DistantPlanet"),
-        UIImage(named: "DoubleQuasarsMergingGalaxies"),
-        UIImage(named: "FamousBlackHole"),
-        UIImage(named: "GalacticDuo"),
-        UIImage(named: "GalaxyInDazzlingDetail"),
-        UIImage(named: "GalaxyWithFaintThreads"),
-        UIImage(named: "HiddenSecret"),
-        UIImage(named: "LegacyFellows"),
-        UIImage(named: "MissingDarkMatters"),
-        UIImage(named: "NASAHubbleSpaceTelescope"),
-        UIImage(named: "Nebula"),
-        UIImage(named: "StellarFurnace"),
-        UIImage(named: "VeilNebula"),
-        UIImage(named: "angryCat")
+    static let photoArray: [UIImage] = [
+        UIImage(named: "BabyStarTantrums")!,
+        UIImage(named: "BlackEyeGalaxy")!,
+        UIImage(named: "CaptivatingSpiral")!,
+        UIImage(named: "ClusterOfRedWhiteBlue")!,
+        UIImage(named: "CometNearJupitersAsteroids")!,
+        UIImage(named: "CosmicNecklace")!,
+        UIImage(named: "DazzlingDynamicDuo")!,
+        UIImage(named: "DistantPlanet")!,
+        UIImage(named: "DoubleQuasarsMergingGalaxies")!,
+        UIImage(named: "FamousBlackHole")!,
+        UIImage(named: "GalacticDuo")!,
+        UIImage(named: "GalaxyInDazzlingDetail")!,
+        UIImage(named: "GalaxyWithFaintThreads")!,
+        UIImage(named: "HiddenSecret")!,
+        UIImage(named: "LegacyFellows")!,
+        UIImage(named: "MissingDarkMatters")!,
+        UIImage(named: "NASAHubbleSpaceTelescope")!,
+        UIImage(named: "Nebula")!,
+        UIImage(named: "StellarFurnace")!,
+        UIImage(named: "VeilNebula")!,
+        UIImage(named: "angryCat")!
     ]
     
     static let photoArrayReversed = Array(photoArray.reversed())
